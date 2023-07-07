cd392a15d268f0ae253ae0eba8b1fde2187d4529
Cverkade <verkadecornelis@gmail.com>
2023-07-07 11:35:34 +0200

    add myRef

--- personal-project/src/FindWorkout.tsx
+++ personal-project/src/FindWorkout.tsx
@@ -1,15 +1,17 @@
 import axios from "axios";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import {exercise} from "./Exercise.tsx";
 import {map} from "react-bootstrap/ElementChildren";
 
 
 const findWorkout = () => {
-
     const [savedWorkout, setSavedWorkout] = useState<exercise[]>([])
     const [workoutFound, setWorkoutFound] = useState<boolean>(false)
+    const myRef = useRef<HTMLInputElement>(null);
+    let workoutId = 0;
     const findWorkoutById = async (e: any) => {
         e.preventDefault()
+        workoutId = e.target.workoutId.value
         try {
             const response = await axios.get(`http://localhost:8080/exercises/workout/` + e.target.workoutId.value)
             setSavedWorkout(response.data);
@@ -23,14 +25,14 @@ const findWorkout = () => {
     return <>
         <form onSubmit={findWorkoutById}>
             <label htmlFor="fname">Search for a workout by ID</label><br/>
-            <input type="search" id="workoutId"/><br/>
+            <input ref={myRef} type="search" id="workoutId"/><br/>
                     <input type="submit" value="Submit"/>
         </form>
         <br/>
         <div className="workoutContainer">
         {workoutFound && (
             <div className={"savedWorkout"}>
-                <h5>Saved workout</h5>
+                <h5>Saved workout {myRef.current.value}</h5>
                 {savedWorkout.map(exercise => <li>{exercise.name}</li>)}
             </div>
         )}
